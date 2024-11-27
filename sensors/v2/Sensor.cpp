LineageOS/android_hardware_xiaomi

sensors: Let the reading of poll fd be configurable
Change-Id: I554a238c11a87d89687b60d3f39446c8f2ff7e2a

sensors/Sensor.cpp
@@ -324,7 +324,7 @@ void SysfsPollingOneShotSensor::run() {
                 continue;
             }
 
-            if (mPolls[1].revents == mPolls[1].events && readBool(mPollFd, true /* seek */)) {
+            if (mPolls[1].revents == mPolls[1].events && readFd(mPollFd)) {
                 activate(false, false, false);
                 mCallback->postEvents(readEvents(), isWakeUpSensor());
             } else if (mPolls[0].revents == mPolls[0].events) {
@@ -355,6 +355,10 @@ std::vector<Event> SysfsPollingOneShotSensor::readEvents() {
 void SysfsPollingOneShotSensor::fillEventData(Event& event) {
     event.u.data[0] = 0;
     event.u.data[1] = 0;
+}
+
+bool SysfsPollingOneShotSensor::readFd(const int mPollFd) {
+    return readBool(mPollFd, true /* seek */);
 }
 
 }  // namespace implementation

PixelLineage/hardware_xiaomi

sensors/v2/Sensor.cpp
@@ -324,7 +324,7 @@ void SysfsPollingOneShotSensor::run() {
                 continue;
             }
 
-            if (mPolls[1].revents == mPolls[1].events && readBool(mPollFd, true /* seek */)) {
+            if (mPolls[1].revents == mPolls[1].events && readFd(mPollFd)) {
                 activate(false, false, false);
                 mCallback->postEvents(readEvents(), isWakeUpSensor());
             } else if (mPolls[0].revents == mPolls[0].events) {
@@ -355,6 +355,10 @@ std::vector<Event> SysfsPollingOneShotSensor::readEvents() {
 void SysfsPollingOneShotSensor::fillEventData(Event& event) {
     event.u.data[0] = 0;
     event.u.data[1] = 0;
+}
+
+bool SysfsPollingOneShotSensor::readFd(const int fd) {
+    return readBool(fd, true /* seek */);
 }
 
 }  // namespace implementation